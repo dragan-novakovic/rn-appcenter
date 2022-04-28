36cb1cf4acc1b96dad119029b9d92bce8d057faf
dragan1810 <d.novakovic1810@yahoo.com>
2022-04-28 15:06:30 +0200

    ADD

--- AppCenterReactNativeShared/ios/post-clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Update pod repository"
-pod repo update
-echo "Installing pods for AppCenterReactNativeShared in `pwd`"
-pod install
--- AppCenterReactNativeShared/prepare-local-podspec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build AppCenterReactNativeShared.framework
-(cd $SRCROOT; $SRCROOT/build-xcframework.sh)
-
-# Generate AppCenter-SDK-ReactNative-iOS-Pod-{version}.zip
-podspecFile=local.podspec $SRCROOT/../zip-framework.sh
